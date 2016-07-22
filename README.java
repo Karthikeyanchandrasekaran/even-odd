0cf08e3bc103bd3179227be77a81fb36ba5d4141
Karthikeyanchandrasekaran <karthik.nany@gmail.com>
2016-07-22 15:00:07 +0530

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,16 @@
+package reve;
+
+import java.util.*;
+
+public class str {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 String s=sc.nextLine();
+		 StringBuffer sb=new StringBuffer(s);
+		 sb.reverse();
+		 System.out.println(sb);
+
+
+}
+}
